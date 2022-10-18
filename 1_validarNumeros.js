fb8a64af25aac1ecbeb144f88342cec961a65455
Eduard <110898623+eduardtomas1@users.noreply.github.com>
2022-10-18 10:59:19 +0200

    Add files via upload

---
+++ 1_validarNumeros.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded',f_main);
+
+function f_main ()
+{
+    document.getElementById('boto1').addEventListener('click', f_);
+    // let numero = document.getElementById('entrada').value;
+    // alert (numero);
+}
+
+function f_validarReal ()
+{
+    
+}
+
+function f_validarEnter ()
+{
+
+}
+
+function f_validarNatural ()
+{
+
+}
